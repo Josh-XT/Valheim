03ef3cdbd2cdece02cba3f68c34a9ef097ce52d7
Josh-XT <josh@devxt.com>
2023-02-13 12:36:49 -0500

    Automatic Backup

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -13,9 +13,11 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
 sudo service docker start
-# Check if Midgard.db and Midgard.fwl exist
-if [ ! -f "config/worlds/Midgard.db" ] || [ ! -f "config/worlds/Midgard.fwl" ]; then
-  # If they don't, copy Midgard.db.old and Midgard.fwl.old from config/worlds
-  cp config/worlds/Midgard.db.old config/worlds/Midgard.db
-  cp config/worlds/Midgard.fwl.old config/worlds/Midgard.fwl
+# Get the value of the "WORLD_NAME" variable from the .env file
+WORLD_NAME=$(grep "WORLD_NAME" .env | cut -d '=' -f2)
+# Check if "$WORLD_NAME.db" and "$WORLD_NAME.fwl" exist
+if [ ! -f "$WORLD_NAME.db" ] || [ ! -f "$WORLD_NAME.fwl" ]; then
+  # If they don't, copy "$WORLD_NAME.db.old" and "$WORLD_NAME.fwl.old" from config/worlds
+  cp config/worlds/"$WORLD_NAME.db.old" "$WORLD_NAME.db"
+  cp config/worlds/"$WORLD_NAME.fwl.old" "$WORLD_NAME.fwl"
 fi
