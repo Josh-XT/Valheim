2db8e37782076d958b09890fbeb9b236967dff01
Josh XT <josh@devxt.com>
2023-02-13 15:41:17 -0500

    Updates

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This setup assumes that the server that it is being installed on is a fresh ``Ub
 I recommend forking this repository if you want to use this on your own server so that you can update the instructions for your own server/world.  If you fork the repository and follow the instructions below, your world will automatically back up to your GitHub repository hourly.
 
 ## Step 1 - Set up Firewall Rules
-Open network firewall ports 2456, 2457, 2458, 27060 and forward to the server's IP address.
+Allow network traffic through firewall ports ``2456-2457`` and forward them to the server's IP address.
 
 ## Step 2 - Clone my repository
 Replace my repository with your URL of your forked repository.
@@ -32,8 +32,6 @@ Change ``SERVER_PUBLIC`` if you want your server to be public.  Ours is private.
 
 Replace ``GITHUB_REPO`` with your GitHub repository that you forked from this one.
 
-Replace ``GITHUB_BRANCH`` with the branch you're using, if you don't know this, use ``main`` like the example has.
-
 Replace ``GITHUB_USER`` with your GitHub username.
 
 Replace ``GITHUB_PASSWORD`` with your personal access token.  [Set up a GitHub personal access token](https://github.com/settings/tokens?type=beta) if you don't already have one.
@@ -52,7 +50,6 @@ BACKUPS_MAX_AGE=1
 ADMINLIST_IDS=76561197973374722 76561197987529611 76561198392805374
 PERMITTEDLIST_IDS=76561197973374722 76561197987529611 76561198392805374
 GITHUB_REPO=github.com/Josh-XT/Valheim.git
-GITHUB_BRANCH=main
 GITHUB_USER=josh-xt
 GITHUB_PASSWORD=PASSWORD
 ```
--- ServerSetup.sh
+++ ServerSetup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
+GITHUB_REPO=$(grep "GITHUB_REPO" .env | cut -d '=' -f2)
+GITHUB_USER=$(grep "GITHUB_USER" .env | cut -d '=' -f2)
+GITHUB_PASSWORD=$(grep "GITHUB_PASSWORD" .env | cut -d '=' -f2)
+WORLD_NAME=$(grep "WORLD_NAME" .env | cut -d '=' -f2)
 sudo ufw allow 2456/udp
 sudo ufw allow 2457/udp
 sudo ufw allow 2458/udp
 sudo ufw allow 27060/udp
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt install ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
-sudo service docker start
-GITHUB_REPO=$(grep "GITHUB_REPO" .env | cut -d '=' -f2)
-GITHUB_BRANCH=$(grep "GITHUB_BRANCH" .env | cut -d '=' -f2)
-GITHUB_USER=$(grep "GITHUB_USER" .env | cut -d '=' -f2)
-GITHUB_PASSWORD=$(grep "GITHUB_PASSWORD" .env | cut -d '=' -f2)
-WORLD_NAME=$(grep "WORLD_NAME" .env | cut -d '=' -f2)
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo snap refresh
+# Check if Docker is already installed
+if ! dpkg -s docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose > /dev/null 2>&1; then
+  sudo apt install ca-certificates curl gnupg lsb-release
+  mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
+  sudo service docker start
+fi
 # Add a cron job to backup the world every hour to GitHub repository
-(crontab -l ; echo "0 * * * * cd $HOME/Valheim/ && git add . && git commit -a -m 'Automatic Backup' && git push --force https://$GITHUB_USER:$GITHUB_PASSWORD@$GITHUB_REPO $GITHUB_BRANCH") | crontab -
+(crontab -l ; echo "0 * * * * cd $HOME/Valheim/ && git add . && git commit -a -m 'Automatic Backup' && git push --force https://$GITHUB_USER:$GITHUB_PASSWORD@$GITHUB_REPO main") | crontab -
 # Check if "$WORLD_NAME.db" and "$WORLD_NAME.fwl" exist
 if [ ! -f "$WORLD_NAME.db" ] || [ ! -f "$WORLD_NAME.fwl" ]; then
   # If they don't, copy "$WORLD_NAME.db.old" and "$WORLD_NAME.fwl.old" from config/worlds
